61fb593696eef8655c5ec03a81b8b9a00d6be4b0
Trenton Daniels <trentondaniels95@gmail.com>
2018-08-10 15:52:33 -0500

    created UserInterface class

---
+++ SweepStakes/UserInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace SweepStakes
+{
+    public static class UserInterface
+    {
+
+        public static string GetFirstName()
+        {
+            string firstName;
+            Console.WriteLine("Hello Contestant. What is your first name?");
+            firstName = Console.ReadLine();
+            return firstName;
+        }
+
+        public static string GetLastName()
+        {
+            string lastName;
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+            return lastName;
+        }
+        public static string GetEmail()
+        {
+            string email;
+            Console.WriteLine("What is your email address?");
+            email = Console.ReadLine();
+            return email;
+        }
+
+        public static int GetRegistrationNumber(int currentRegistrationNumber)
+        {
+            currentRegistrationNumber++;
+            return currentRegistrationNumber;
+        }
+    }
+}
